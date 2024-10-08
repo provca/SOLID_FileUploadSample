1d4467e4e8fd0cb7ac4448a0432259860df6aef4
Pere Rovira <122680815+provca@users.noreply.github.com>
2024-10-08 15:30:41 +0200

    Create home and upload-file-byte-array. Assign routes for navigation
    Creating the home and upload-file-byte-array components.
    Registering the components in app.modules.
    Setting the app component as home.
    Moving the html/css content from app to home.
    Creating the button that takes us to the upload-file-byte-array html.
    Assign routes for navigation in app-routing.
    Formatting the code to accept 'eslint' (prettier/prettier).

--- Apps/AngularWebApp/AngularWebApp/src/app/app-routing.module.ts
+++ Apps/AngularWebApp/AngularWebApp/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+//import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { UploadFileByteArrayComponent } from "./components/upload-file-byte-array/upload-file-byte-array.component";
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "home", component: HomeComponent },
+    { path: "uploadfile", component: UploadFileByteArrayComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ Apps/AngularWebApp/AngularWebApp/src/app/components/upload-file-byte-array/upload-file-byte-array.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-upload-file-byte-array",
+    templateUrl: "./upload-file-byte-array.component.html",
+    styleUrl: "./upload-file-byte-array.component.css"
+})
+export class UploadFileByteArrayComponent {
+
+}
